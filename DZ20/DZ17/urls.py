8f6613029df7a1b5cdf3d363bdfb85520f6aac7c
hitman4ik85 <hitman4ik85@gmail.com>
2025-04-29 01:31:04 +0300

    DZ20

---
+++ DZ20/DZ17/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from store import views
+
+urlpatterns = [
+    path('', views.index),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('products/', views.products),
+    path('delivery/', views.delivery),
+    path('cart/', views.cart),
+    path('orders/', views.orders),
+    path('admin/', views.admin, name='admin'),
+    path('addProduct/', views.addProduct, name='addProduct'),
+    path('editProduct/<int:id>/', views.editProduct, name='editProduct'),
+    path('deleteProduct/<int:id>/', views.deleteProduct, name='deleteProduct'),
+    path('viewProduct/', views.viewProduct, name='viewProduct'),
+]
